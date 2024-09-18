88ab1def49e18664f644605b3387cb5eb0693867
shu-ken <shu@hanri.jp>
2024-09-19 07:53:54 +0900

    zodResolverを導入してスキーマの型定義を適用させる

--- components/Mailform/Mailform.tsx
+++ components/Mailform/Mailform.tsx
@@ -4,9 +4,20 @@ import { Form, FormControl, FormDescription, FormField, FormItem, FormLabel, For
 import { Button } from '../ui/button'
 import { Input } from '../ui/input'
 import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { formSchema } from '@/lib/formSchema'
+import { Textarea } from '../ui/textarea'
 
 const Mailform = () => {
-  const form = useForm()
+  const form = useForm({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      username: '',
+      subject: '',
+      email: '',
+      content: '',
+    },
+  })
 
   function onSubmit(values: any) {
     console.log(values)
@@ -24,11 +35,49 @@ const Mailform = () => {
           name="username"
           render={({ field }) => (
             <FormItem>
-              <FormLabel>Username</FormLabel>
+              <FormLabel>ユーザー名</FormLabel>
               <FormControl>
-                <Input placeholder="shadcn" {...field} />
+                <Input placeholder="ユーザー名" {...field} />
               </FormControl>
-              <FormDescription>This is your public display name.</FormDescription>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>メールアドレス</FormLabel>
+              <FormControl>
+                <Input placeholder="メールアドレス" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="subject"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>主題</FormLabel>
+              <FormControl>
+                <Input placeholder="主題" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="content"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>本文</FormLabel>
+              <FormControl>
+                <Textarea placeholder="本文" {...field} />
+              </FormControl>
               <FormMessage />
             </FormItem>
           )}
