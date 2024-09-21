a534d52d7600ac4cd6e4fc31fd40a4a0dbcb0017
shu-ken <shu@hanri.jp>
2024-09-21 18:50:29 +0900

    ファイル送信項目をお問い合わせフォームに追加する

--- components/Mailform/Mailform.tsx
+++ components/Mailform/Mailform.tsx
@@ -63,6 +63,20 @@ const Mailform = () => {
             </FormItem>
           )}
         />
+
+        <FormField
+          control={form.control}
+          name="file"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>添付画像</FormLabel>
+              <FormControl>
+                <Input accept="image/" type="file" placeholder="主題" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
         <Button type="submit">Submit</Button>
       </form>
     </Form>
